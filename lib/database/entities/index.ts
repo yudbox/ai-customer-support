63d125c29c61d81d1a8208926c78aacfff69bb5e
Oleksandr Yudaiev <oyudaiev@partner.sephora.eu>
2026-03-24 15:59:20 +0200

    fix: use export type for enums and interfaces in entities index

--- lib/database/entities/index.ts
+++ lib/database/entities/index.ts
@@ -1,8 +1,19 @@
-export { Customer, CustomerTier } from "./Customer";
-export { Order, OrderStatus, OrderItem } from "./Order";
-export { Ticket, TicketStatus, TicketPriority, SentimentLabel } from "./Ticket";
-export { Shipment, ShipmentStatus, ShipmentEvent } from "./Shipment";
+export { Customer } from "./Customer";
+export type { CustomerTier } from "./Customer";
+
+export { Order } from "./Order";
+export type { OrderStatus, OrderItem } from "./Order";
+
+export { Ticket } from "./Ticket";
+export type { TicketStatus, TicketPriority, SentimentLabel } from "./Ticket";
+
+export { Shipment } from "./Shipment";
+export type { ShipmentStatus, ShipmentEvent } from "./Shipment";
+
 export { Product } from "./Product";
 export { Category } from "./Category";
 export { Team } from "./Team";
-export { Refund, RefundStatus } from "./Refund";
+
+export { Refund } from "./Refund";
+export type { RefundStatus } from "./Refund";
+
